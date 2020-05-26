268a706f54db8f968fb945663da8c50bd1fe26c9
Ruhul Amin <ruhulinfo50@gmail.com>
2020-05-26 23:02:36 +0600

    added listing

--- listings/views.py
+++ listings/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 # Create your views here.
 
 def index(request):
-    listings = Listing.objects.all()
-    paginator = Paginator(listings,3)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings,2)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
+
     contex = {
         'listings':paged_listings
     }
     return render(request,'listings/listings.html',contex)
 
 def listing(request,listing_id):
-    return render(request,'listings/listing.html')
+    listing = get_object_or_404(Listing,pk=listing_id)
+    contex = {
+        'listing':listing
+    }
+    return render(request,'listings/listing.html',contex)
 
 
 def search(request):
